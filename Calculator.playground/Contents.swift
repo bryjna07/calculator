30b444938cfb2bbd761c6c09f9e963dd9dce70f2
Youngjin <bryjna07@gmail.com>
2024-10-30 17:39:33 +0900

    [modify] switch 문 사용해보기

--- Calculator.playground/Contents.swift
+++ Calculator.playground/Contents.swift
@@ -4,50 +4,53 @@
 
 
 class Calculator {
-    // Todo : 내부 구현하기
-    var firstNumber: Double
-    var secondNumber: Double
+     // Todo : 내부 구현하기
+    var num1: Double
+    var num2: Double
+    var opp: String
     
-    init(firstNumber: Double, secondNumber: Double) {
-        self.firstNumber = firstNumber
-        self.secondNumber = secondNumber
+    init(num1: Double, num2: Double, opp: String) {
+        self.num1 = num1
+        self.num2 = num2
+        self.opp = opp
     }
     
-    // 더하기
-    func add() -> Double {
-       return firstNumber + secondNumber
-    }
-    
-    //빼기
-    func subtract() -> Double {
-        return firstNumber - secondNumber
-    }
-    
-    //곱하기
-    func multiply() -> Double {
-        return firstNumber * secondNumber
-    }
-    
-    //나누기
-    func divide() -> Double {
-        return firstNumber / secondNumber
+    func calculate(opp: String, num1: Double, num2: Double) -> Double? {
+    switch opp {
+            // 더하기
+        case "+" :
+            //        func add() -> Double {
+            return num1 + num2
+            //        }
+            
+            //빼기
+        case "-" :
+            //        func subtract() -> Double {
+            return num1 - num2
+            //        }
+            
+            //곱하기
+        case "*" :
+            //        func multiply() -> Double {
+            return num1 * num2
+            //        }
+            
+            //나누기
+        case "/" :
+            //        func divide() -> Double {
+            return num1 / num2
+            //        }
+        default:
+            return nil
+       
+        }
     }
 }
     
-let addition = Calculator(firstNumber: 3, secondNumber: 6)
-print(addition.add())
-
-let subtraction = Calculator(firstNumber: 8, secondNumber: 4)
-print(subtraction.subtract())
-
-let multiplication = Calculator(firstNumber: 3, secondNumber: 2)
-print(multiplication.multiply())
-
-let division = Calculator(firstNumber: 6, secondNumber: 4)
-print(division.divide())
-
 //다시, 이 브랜치로 작업
+//switch문과 opertor 활용해보기
 
-
-
-
+let calculator = Calculator(num1: 6, num2: 4, opp: "+")
+if let calculator = calculator.calculate(opp: "+", num1: 6, num2: 4) {
+    print(calculator)
+}
