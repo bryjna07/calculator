fe39d90309f5335309750a206d3d24dc418120dc
Youngjin <bryjna07@gmail.com>
2024-10-31 11:21:55 +0900

    [modify] 이전 코드 수정 파라미터 이름, switch 문 사용 전 내용 정리

--- Calculator.playground/Contents.swift
+++ Calculator.playground/Contents.swift
@@ -9,39 +9,37 @@ class Calculator {
     var num2: Double
     var opp: String
     
-    init(num1: Double, num2: Double, opp: String) {
+    init(_ num1: Double, _ num2: Double, _ opp: String) {
         self.num1 = num1
         self.num2 = num2
         self.opp = opp
     }
     
-    func calculate(opp: String, num1: Double, num2: Double) -> Double? {
+    func calculate(_ num1: Double, _ num2: Double, _ opp: String) -> Double? {
     switch opp {
             // 더하기
         case "+" :
-            //        func add() -> Double {
             return num1 + num2
-            //        }
-            
+        
             //빼기
         case "-" :
-            //        func subtract() -> Double {
             return num1 - num2
-            //        }
             
             //곱하기
         case "*" :
-            //        func multiply() -> Double {
             return num1 * num2
-            //        }
             
             //나누기
         case "/" :
-            //        func divide() -> Double {
-            return num1 / num2
-            //        }
-        default:
+        // 0으로 나누기 불가 -> num2 0 일 때 설정
+        guard num2 != 0 else {
+            print("0으로 나눌 수 없습니다.")
             return nil
+        }
+        return num1 / num2
+    default:
+        print("잘못된 연산자 입니다.")
+        return nil
        
         }
     }
@@ -50,7 +48,15 @@ class Calculator {
 //다시, 이 브랜치로 작업
 //switch문과 opertor 활용해보기
 
-let calculator = Calculator(num1: 6, num2: 4, opp: "+")
-if let calculator = calculator.calculate(opp: "+", num1: 6, num2: 4) {
-    print(calculator)
+//let calculator = Calculator(num1: 6, num2: 4, opp: "+")
+//if let calculator = calculator.calculate(opp: "+", num1: 6, num2: 4) {
+//    print(calculator)
+//}
+//let testNumber = Calculator(num1: 4, num2: 0, opp: "/")
+//if let testNumber = testNumber.calculate(opp: "/", num1: 4, num2: 0) {
+//    print(testNumber)
+//}
+let calculator = Calculator(4, 6, "+")
+if let addfuntion = calculator.calculate(4, 6, "+") {
+    print(addfuntion)
 }
