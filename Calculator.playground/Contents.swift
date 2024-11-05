48996dcb6131c57e5da1e7e95b0a2a741de61bd7
Youngjin <bryjna07@gmail.com>
2024-11-05 11:41:41 +0900

    [modify] 피드백 수정 과정

--- Calculator.playground/Contents.swift
+++ Calculator.playground/Contents.swift
@@ -2,6 +2,11 @@
 // 더하기, 빼기, 나누기, 곱하기 연산을 수행할 수 있는 Calculator 클래스를 만들기
 // 생성한 클래스를 이용하여 연산을 진행하고 출력
 
+// 에러처리를 위한 열거형 정의
+enum CalculatorError: Error {
+    case divideByZero
+    case invalidOperator
+}
 
 class Calculator {
     // 연산 클래스 추가
@@ -11,7 +16,7 @@ class Calculator {
     let divideOperation = DivideOperation()
     
     
-    func calculate(_ num1: Double, _ num2: Double, _ opp: String) -> Double? {
+    func calculate(_ num1: Double, _ num2: Double, _ opp: String) -> Double {
     switch opp {
     // 더하기
         case "+" :
@@ -29,23 +34,34 @@ class Calculator {
         case "/" :
         // 0으로 나누기 불가 -> num2 0 일 때 설정
         guard num2 != 0 else {
-            print("0으로 나눌 수 없습니다.")
-            return nil
+          //  print("0으로 나눌 수 없습니다.")     ---> do-try-catch 에러처리 사용
+            throw CalculatorError.divideByZero
+//            return nil
         }
         return num1 / num2
         // 나머지 연산자 추가
     case "%" :
         return num1.truncatingRemainder(dividingBy: num2)
     default:
-        print("잘못된 연산자 입니다.")  // 옵셔널로 반환되기 때문에 안내멘트 작성
-        return nil
-       
+//        print("잘못된 연산자 입니다.")  // 옵셔널로 반환되기 때문에 안내멘트 작성
+//        return nil
+        throw CalculatorError.invalidOperator
         }
-    }
 }
     
 //다시, 이 브랜치로 작업
 //switch문과 opertor 활용해보기
+    
+    do {
+        let result = try calculator.calculate(4, 6, "+")
+        print("Result:\(result)")
+    } catch CalculatorError.divideByZero {
+        print("0으로 나눌 수 없습니다.")
+    } catch CalculatorError.invalidOperator {
+        print("잘못된 연산자 입니다.")
+    } catch {
+        print("알 수 없는 에러가 발생했습니다.")
+    }
 
 let calculator = Calculator()
 if let addfuntion = calculator.calculate(4, 6, "+") {
@@ -67,21 +83,21 @@ if let zeroResult = zeroTest.calculate(5, 0, "/") {
 // 각각의 연산 클래스 생성
 class AddOperation {
     func calculate(_ num1: Double, _ num2: Double, _ opp: String) -> Double {
-        var opp = "+"
+//        var opp = "+"             없어도됨
         return num1 + num2
     }
 }
 
 class SubtractOperation {
     func calculate(_ num1: Double, _ num2: Double, _ opp: String) -> Double {
-        var opp = "-"
+//        var opp = "-"
         return num1 - num2
     }
 }
 
 class MultiplyOperation {
     func calculate(_ num1: Double, _ num2: Double, _ opp: String) -> Double {
-        var opp = "*"
+//        var opp = "*"
         return num1 * num2
     }
 }
@@ -89,8 +105,9 @@ class MultiplyOperation {
 class DivideOperation {
     func calculate(_ num1: Double, _ num2: Double, _ opp: String) -> Double? {
         guard num2 != 0 else {
-            print("0으로 나눌 수 없습니다.")
-            return nil
+            throw CalculatorError.divideByZero
+//            print("0으로 나눌 수 없습니다.")
+//            return nil
         }
         return num1 / num2
     }
